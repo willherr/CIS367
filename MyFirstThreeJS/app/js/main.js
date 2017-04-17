5482db9e15b4862910154a1f60179b48eeeccfb9
Vincent Ball <ballv@mail.gvsu.edu>
2017-04-16 21:22:43 -0400

    Added game over modal

--- MyFirstThreeJS/app/js/main.js
+++ MyFirstThreeJS/app/js/main.js
@@ -283,6 +283,7 @@ function countdown() {
         try {
             clearTimeout(timerId);
             window.removeEventListener("click", clickFruitEvent, false);
+            displayModal();
         } catch (Exception) {
             //remove from logger the exception when time is up
         }
@@ -304,3 +305,28 @@ function randomRotation(index){
     return lastRotation[index];
 }
 
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+function displayModal() {
+    modal.style.display = "block";
+    document.getElementById('score_final').innerHTML = "Your score was: " + score;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
